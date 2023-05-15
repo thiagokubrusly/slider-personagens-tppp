8c6702e5d37327cad11da43ab250e9beb5799cfc
Thiago Kubrusly <thiago.kfreitas@hotmail.com>
2023-05-15 12:51:31 -0300

    add feature de passar cartoes usando keyboard

--- js/main.js
+++ js/main.js
@@ -42,86 +42,17 @@ btnVoltar.addEventListener('click', function (){
 });
 
 
-/*
-CÓDIGO PENSADO COM DANI
-
-const btnAvancar = document.getElementById('btn-avancar');
-const btnVoltar = document.getElementById('btn-voltar');
-const cartoes = document.querySelectorAll('.cartao');
-let cartaoAtual = 0;
-
-function esconderCartaoSelecionado(){
-    const cartaoSelecionado = document.querySelector('.selecionado');
-    cartaoSelecionado.classList.remove('selecionado');
+// ATIVAÇÃO DAS SETAS PELO TECLADO
+const mapaTeclado = {
+'ArrowLeft': 'btn-voltar',
+'ArrowRight': 'btn-avancar',
 }
 
-function mostrarCartao(indiceCartao){
-    cartoes[indiceCartao].classList.add('selecionado');
-    //cartoes[0]
-
-}
-
-btnAvancar.addEventListener('click', function (){
-    if(cartaoAtual===cartoes.length - 1){ //no projeto original, esse if servia apenas para mostrar o última cartão da lengh.
-        
-        cartaoAtual = 0; //invoca a variável criada que mostra o primeiro cartão da nossa lenght, definindo novamente - a partir dessa linha - o cartaoAtual sendo o primeiro cartão da length (cujo índice é 0).
-
-        esconderCartaoSelecionado(); //ao invocar essa função, esconde o cartão selecionado, que no caso, era o último da nossa lenght.
-        
-        mostrarCartao(cartaoAtual); //invoca a função que mostra o cartão selecionado, mas agora com o objeto sendo o cartãoAtual = 0.
-
-        return; //encerra o if e apresenta o que foi passado no if.
-    } 
-
-    esconderCartaoSelecionado();
-    cartaoAtual++;
-    mostrarCartao(cartaoAtual);
-});
-
-btnVoltar.addEventListener('click', function (){ 
-    if(cartaoAtual === 0) { //mesma lógica do btnAvancar, mas com "engenharia reversa". 
-
-        cartaoAtual = cartoes.length - 1; //pelo cartaoAtual (lá no início do código) ter sido uma variável declarada com let, ela pode ser ressignificada e receber um novo valor diversas vezes. Aqui, foi definido para ser o último cartão da lengh.
-
-        esconderCartaoSelecionado(); //ao invocar essa função, esconde o cartão selecionado, que aqui, era o primeiro da nossa lenght.
-
-        mostrarCartao(cartaoAtual); //invoca a função que mostra o cartão selecionado, mas agora com o objeto sendo o cartãoAtual = cartoes.length - 1.
-
-        return; //encerra o if e apresenta o que foi passado no if.
-    }
-
-    esconderCartaoSelecionado();
-    cartaoAtual--;
-    mostrarCartao(cartaoAtual);
-});
-
-*/
-
-
-
-
-
-
-/*
-PASSO A PASSO DO PENSAMENTO:
-
-OBJETIVO 1 - quando clicarmos na seta de avançar, temos que mostrar o próximo cartão.
+const ativandoTeclado = (event) => {
     
-    PASSO 1: dar um jeito de pegar o elementos HTML da seta avancar;
+    const tecla = event.key;
+    const teclaEscolhida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1;
+    if (teclaEscolhida()) document.getElementById(mapaTeclado[tecla]).click();
+};
 
-    PASSO 2: dar um jeito de identificar o clique do usuário na seta avançar;
-
-    PASSO 3: fazer aparecer o próximo cartão da lista;
-
-    PASSO 4: buscar o cartão selecionado e esconder.
-
-OBJETIVO 2 - quando clicarmos na seta de voltar, temos que mostrar o cartão anterior.
-
-    PASSO 1: dar um jeito de pegar o elementos HTML da seta voltar;
-
-    PASSO 2: dar um jeito de identificar o clique do usuário na seta voltar;
-
-    PASSO 3: fazer aparecer o cartão anterior da lista;
-
-    PASSO 4: buscar o cartão selecionado e esconder.
-*/
+document.addEventListener('keydown', ativandoTeclado);
